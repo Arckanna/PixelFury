42723266d0e02c5cf5bfeea8b0ff3398730debe6
Arcka <valerie.lecoeur75@gmail.com>
2024-03-29 19:23:09 +0100

    Bug correction

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -4,30 +4,16 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    [SerializedField] EnemyController enemyPrefab;
-    [SerializedField] float spawnRate;
-    [SerializedField] List<Transform> spawnPosition;
-
-    private float spawnTimer = 0;
     
     
 
     // Update is called once per frame
     void Update()
     {
-        if(spawnTimer <= 0)
-        {
-            spawnTimer = spawnRate;
-            SpawnEnemy();
-        }
-        else
-        {
-            spawnTimer -= Time.deltaTime;
-        }
+       
     }
     void SpawnEnemy()
     {
-        Transform randomSpawnPosition = spawnPosition[Random.Range(0, spawnPosition.Count)];
-        Instantiate(enemyPrefab, randomSpawnPosition.position, Quaternion.identity);
+        
     }
 }
