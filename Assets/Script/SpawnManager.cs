97d3314a006572da0b07a3f4d2b7cbaa0758f5a6
Arcka <valerie.lecoeur75@gmail.com>
2024-03-29 21:19:22 +0100

    SpawnManager

--- Assets/Script/SpawnManager.cs
+++ Assets/Script/SpawnManager.cs
@@ -4,16 +4,31 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
-    
+    [SerializeField] EnemyController enemyController;
+    [SerializeField] float spawnRate;
+    [SerializeField] float spawnRadius;
+    [SerializeField] Transform spawnPosition;
+
+    // Start is called before the first frame update
+    float spawnTimer;
     
 
     // Update is called once per frame
     void Update()
     {
-       
+       spawnTimer -= Time.deltaTime;
+        if (spawnTimer <= 0)
+        {
+              SpawnEnemy();
+              spawnTimer = spawnRate;
+         }
     }
     void SpawnEnemy()
     {
-        
+        Vector3 randomDirection = Random.insideUnitSphere * spawnRadius;
+        randomDirection += spawnPosition.position;
+        UnityEngine.AI.NavMeshHit hit;
+        UnityEngine.AI.NavMesh.SamplePosition(randomDirection, out hit, spawnRadius, 1);
+        Instantiate(enemyController, hit.position, Quaternion.identity);
     }
 }
